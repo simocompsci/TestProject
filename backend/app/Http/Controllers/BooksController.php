b87ee809eb84eb4ed5b5f42b6c58708ea905b5f8
simocompsci <hamids9alo7777@gmail.com>
2025-12-08 22:16:11 +0100

    put down the functions of the books cntroller

--- backend/app/Http/Controllers/BooksController.php
+++ backend/app/Http/Controllers/BooksController.php
@@ -6,5 +6,19 @@ use Illuminate\Http\Request;
 
 class BooksController extends Controller
 {
-    //
+    public function getAllBooks(Request $request , $id){
+
+    }
+
+    public function AddBook(Request $request){
+
+    }
+
+    public function UpdateBook(Request $request){
+
+    }
+
+    public function DeleteBook(Request $request){
+        
+    }
 }
